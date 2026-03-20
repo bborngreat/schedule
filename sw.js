e6c5e2e4ca130c2cf35a09f31feed11acdc73f1a
Bright Borngreat <borngreatbright019@gmail.com>
2026-03-20 09:29:53 +0000

    Version 2.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'schedule-v1';
+const CACHE_NAME = 'schedule-v2';
 const ASSETS_TO_CACHE = [
   './',                       // this will usually serve to-do.html
   './index.html',
